54235c300ffed5f68e55f92c9aac2c89217f2ddf
BuckeyeDude_cp <BuckeyeDude@users.noreply.github.com>
2011-06-01 12:34:45 +0000

    Made it an option to not create home dir folders Allowed for customized file locations

--- Wabbitcode/Classes/FileLocations.cs
+++ Wabbitcode/Classes/FileLocations.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
+using Revsoft.Wabbitcode.Properties;
 using System.Windows.Forms;
 
 namespace Revsoft.Wabbitcode.Classes
@@ -10,26 +8,88 @@ namespace Revsoft.Wabbitcode.Classes
     public static class FileLocations
     {
         public static string WabbitcodeFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Wabbitcode");
-        public static string WabbitemuFile = Path.Combine(WabbitcodeFolder, "Wabbitemu.exe");
-        public static string SpasmFile = Path.Combine(WabbitcodeFolder, "spasm.exe");
-        public static string TemplatesDir = Path.Combine(WabbitcodeFolder, "Templates");
-        public static string IncludesDir = Path.Combine(WabbitcodeFolder, "Includes");
-        public static string ProjectsDir = Path.Combine(WabbitcodeFolder, "Projects");
-        public static string ConfigFile = Path.Combine(WabbitcodeFolder, "DockPanel.config");
+        public static string WabbitemuFile
+		{
+			get
+			{
+				return Settings.Default.wabbitLoc.Replace("%docs%", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+			}
+		}
+		public static string SpasmFile
+		{
+			get
+			{
+				return Settings.Default.assemblerLoc.Replace("%docs%", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+			}
+		}
+        public static string TemplatesDir
+		{
+			get
+			{
+				if (Settings.Default.createFolders)
+					return Path.Combine(WabbitcodeFolder, "Templates");
+				else
+					return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Templates");
+			}
+		}
+        public static string IncludesDir
+		{
+			get
+			{
+				if (Settings.Default.createFolders)
+					return Path.Combine(WabbitcodeFolder, "Includes");
+				else
+					return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Includes");
+			}
+		}
+        public static string ProjectsDir 
+		{
+			get
+			{
+				if (Settings.Default.createFolders)
+					return Path.Combine(WabbitcodeFolder, "Projects");
+				else
+					return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Templates");
+			}
+		}
+		public static string ConfigFile
+		{
+			get
+			{
+				return Settings.Default.configLoc.Replace("%docs%", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+			}
+		}
 
         internal static void InitDirs()
         {
-            try
-            {
-                Directory.CreateDirectory(WabbitcodeFolder);
-                Directory.CreateDirectory(TemplatesDir);
-                Directory.CreateDirectory(IncludesDir);
-                Directory.CreateDirectory(ProjectsDir);
-            }
-            catch (Exception ex)
-            {
-                Services.DockingService.ShowError("Unable to create directories", ex);
-            }
+			if (Settings.Default.createFolders)
+			{
+				try
+				{
+					Directory.CreateDirectory(WabbitcodeFolder);
+					Directory.CreateDirectory(TemplatesDir);
+					Directory.CreateDirectory(IncludesDir);
+					Directory.CreateDirectory(ProjectsDir);
+				}
+				catch (Exception ex)
+				{
+					Services.DockingService.ShowError("Unable to create directories", ex);
+				}
+			}
         }
+
+		internal static void InitFiles()
+		{
+			string dir = Path.GetDirectoryName(SpasmFile);
+			if (!Directory.Exists(dir))
+				if (MessageBox.Show("Directory '" + dir + "' does not exist. Would you like to create it?", "Directory does not exist", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) != DialogResult.Yes)
+					Settings.Default.assemblerLoc = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "spasm.exe");
+			Classes.Resources.GetResource("spasm.exe", FileLocations.SpasmFile);
+			dir = Path.GetDirectoryName(WabbitemuFile);
+			if (!Directory.Exists(dir))
+				if (MessageBox.Show("Directory '" + dir + "' does not exist. Would you like to create it?", "Directory does not exist", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) != DialogResult.Yes)
+					Settings.Default.wabbitLoc = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "wabbitemu.exe");
+			Classes.Resources.GetResource("Wabbitemu.exe", FileLocations.WabbitemuFile);
+		}
     }
 }
--- Wabbitcode/Properties/TempSettings.Designer.cs
+++ Wabbitcode/Properties/TempSettings.Designer.cs
@@ -265,7 +265,7 @@ namespace Revsoft.Wabbitcode.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute(".\\wabbitemu.exe")]
+        [global::System.Configuration.DefaultSettingValueAttribute("%HOMEDIR%\\Wabbitcode\\Wabbitemu.exe")]
         public string wabbitLoc {
             get {
                 return ((string)(this["wabbitLoc"]));
@@ -356,18 +356,6 @@ namespace Revsoft.Wabbitcode.Properties {
             }
             set {
                 this["ptiLoc"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute(".\\vti.exe")]
-        public string vtiLoc {
-            get {
-                return ((string)(this["vtiLoc"]));
-            }
-            set {
-                this["vtiLoc"] = value;
             }
         }
         
@@ -817,18 +805,6 @@ namespace Revsoft.Wabbitcode.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public bool mouseWheelZoom {
-            get {
-                return ((bool)(this["mouseWheelZoom"]));
-            }
-            set {
-                this["mouseWheelZoom"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool recentlyUpdated {
             get {
@@ -836,6 +812,18 @@ namespace Revsoft.Wabbitcode.Properties {
             }
             set {
                 this["recentlyUpdated"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool mouseWheelZoom {
+            get {
+                return ((bool)(this["mouseWheelZoom"]));
+            }
+            set {
+                this["mouseWheelZoom"] = value;
             }
         }
         
@@ -884,6 +872,42 @@ namespace Revsoft.Wabbitcode.Properties {
             }
             set {
                 this["convertTabs"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool createFolders {
+            get {
+                return ((bool)(this["createFolders"]));
+            }
+            set {
+                this["createFolders"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("%HOMEDIR%\\Wabbitcode\\Wabbitemu.exe")]
+        public string assemblerLoc {
+            get {
+                return ((string)(this["assemblerLoc"]));
+            }
+            set {
+                this["assemblerLoc"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string configLoc {
+            get {
+                return ((string)(this["configLoc"]));
+            }
+            set {
+                this["configLoc"] = value;
             }
         }
     }
