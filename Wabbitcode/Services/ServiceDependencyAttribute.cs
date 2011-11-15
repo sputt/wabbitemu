4d37a0815574fa4ef819f9e95e9332f59979fbe1
BuckeyeDude_cp <BuckeyeDude@users.noreply.github.com>
2011-11-15 07:57:22 +0000

    Wabbitcode leftovers

---
+++ Wabbitcode/Services/ServiceDependencyAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Revsoft.Wabbitcode.Services
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple=true)]
+	public class ServiceDependencyAttribute : Attribute
+	{
+		string dependencyName;
+		public ServiceDependencyAttribute(string dependencyName)
+		{
+			this.dependencyName = dependencyName;
+		}
+
+		public string DependencyName
+		{
+			get { return dependencyName; }
+		}
+	}
+}
