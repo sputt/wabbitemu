aa85f81978c50d2b9e81dd4c55edb848395956be
sputt_cp <SND\sputt_cp>
2015-01-22 04:38:08 +0000

    Added tile collisions back in and a new shader specifically for black and white

--- WabbitStudio/Source/WPFZ80MapEditor/WPFZ80MapEditor/Effects/Tint.fx
+++ WabbitStudio/Source/WPFZ80MapEditor/WPFZ80MapEditor/Effects/Tint.fx
@@ -4,8 +4,8 @@ float4 color : register(c0);
 float4 main(float2 uv : TEXCOORD) : COLOR
 {
 	float4 pix = tex2D(input, uv);
-	if (pix.a > 0) {
-		return lerp(pix, color, 0.4);
+	if (pix.a > 0 && !(pix.r < 0.2 && pix.g < 0.2 && pix.b <  0.2)) {
+		return lerp(pix, color, 0.60);
 	} else {
 		return pix;
 	}
--- WabbitStudio/Source/WPFZ80MapEditor/WPFZ80MapEditor/MapView.vb
+++ WabbitStudio/Source/WPFZ80MapEditor/WPFZ80MapEditor/MapView.vb
@@ -29,6 +29,22 @@ Public Class MapView
                            GetType(MapData), GetType(MapView), _
                            New FrameworkPropertyMetadata(Nothing, FrameworkPropertyMetadataOptions.AffectsRender, AddressOf OnMapChanged))
 
+
+    Public Property ShowCollisions As Boolean
+        Get
+            Return GetValue(ShowCollisionsProperty)
+        End Get
+
+        Set(ByVal value As Boolean)
+            SetValue(ShowCollisionsProperty, value)
+        End Set
+    End Property
+
+    Public Shared ReadOnly ShowCollisionsProperty As DependencyProperty = _
+                           DependencyProperty.Register("ShowCollisions", _
+                           GetType(Boolean), GetType(MapView), _
+                           New FrameworkPropertyMetadata(False, FrameworkPropertyMetadataOptions.AffectsRender))
+
     Protected Overrides Sub OnRender(DrawingContext As System.Windows.Media.DrawingContext)
         If Map Is Nothing OrElse Map.Tileset Is Nothing Then Exit Sub
 
@@ -38,7 +54,21 @@ Public Class MapView
             Dim Bounds As New Rect(16 * (i Mod 16), 16 * (Math.Floor(i / 16)), 16, 16)
             DrawingContext.DrawImage(Map.Tileset.Tiles(TileIndex).Image, Bounds)
         Next
+        If ShowCollisions Then
+            DrawingContext.PushOpacity(0.25)
+            Dim Pen As New Pen
+            Pen.Thickness = 0
+            Dim Brush As Brush = Application.Current.Resources("CollisionBrush")
 
+            For i = 0 To Map.TileData.Count - 1
+                Dim TileIndex = Map.TileData(i) Mod 128
+                If Map.TileData(i) > 128 Then
+                    Dim Bounds As New Rect(16 * (i Mod 16), 16 * (Math.Floor(i / 16)), 16, 16)
+                    DrawingContext.DrawRectangle(Brush, Pen, Bounds)
+                End If
+            Next
+            DrawingContext.Pop()
+        End If
     End Sub
 
 End Class
