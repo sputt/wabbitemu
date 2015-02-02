356478f4b8210468f61c7819b4b378ee57ed9330
BuckeyeDude_cp <BuckeyeDude@users.noreply.github.com>
2015-02-02 04:28:17 +0000

    Refactor debug enum to not hardcode the byte value

--- Wabbitcode/GUI/DockingWindows/Tracking/EnumVariableDisplayController.cs
+++ Wabbitcode/GUI/DockingWindows/Tracking/EnumVariableDisplayController.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Revsoft.Wabbitcode.Services.Debugger;
+using Revsoft.Wabbitcode.Services.Interfaces;
 using Revsoft.Wabbitcode.Services.Project;
 using Revsoft.Wabbitcode.Utils;
 
@@ -10,6 +10,8 @@ namespace Revsoft.Wabbitcode.GUI.DockingWindows.Tracking
     internal class EnumVariableDisplayController : ByteDisplayController
     {
         private readonly DebuggingEnum _enum;
+        private readonly ISymbolService _symbolService;
+        private readonly Dictionary<ByteMaskKey, string> _symbolCache = new Dictionary<ByteMaskKey, string>(); 
 
         public override string Name
         {
@@ -20,6 +22,28 @@ namespace Revsoft.Wabbitcode.GUI.DockingWindows.Tracking
             : base(evaluator)
         {
             _enum = debugEnum;
+            _symbolService = DependencyFactory.Resolve<ISymbolService>();
+            var debuggerService = DependencyFactory.Resolve<IDebuggerService>();
+            debuggerService.OnDebuggingStarted += _debuggerService_OnDebuggingStarted;
+        }
+
+        private void _debuggerService_OnDebuggingStarted(object sender, DebuggingEventArgs e)
+        {
+            _symbolCache.Clear();
+            foreach (var maskKey in _enum.EnumMapping)
+            {
+                foreach (var enumVal in maskKey.Value)
+                {
+                    var addressFromLabel = _symbolService.SymbolTable.GetAddressFromLabel(enumVal);
+                    if (addressFromLabel == null)
+                    {
+                        continue;
+                    }
+
+                    byte enumByteVal = (byte) addressFromLabel;
+                    _symbolCache.Add(new ByteMaskKey(enumByteVal, maskKey.Key), enumVal);
+                }
+            }
         }
 
         protected override string GetDisplayValue(IWabbitcodeDebugger debugger, int address, int size)
@@ -27,11 +51,13 @@ namespace Revsoft.Wabbitcode.GUI.DockingWindows.Tracking
             byte readByte = debugger.ReadByte((ushort) address);
 
             var enumValues = new List<string>();
-            foreach (var enumVal in _enum.EnumMapping)
+            foreach (var maskKey in _enum.EnumMapping)
             {
-                if ((readByte & enumVal.Key.Mask) == enumVal.Key.Byte)
+                string label;
+                byte maskedValue = (byte) (readByte & maskKey.Key);
+                if (_symbolCache.TryGetValue(new ByteMaskKey(maskedValue, maskKey.Key), out label))
                 {
-                    enumValues.Add(enumVal.Value);
+                    enumValues.Add(label);
                 }
             }
 
@@ -44,16 +70,31 @@ namespace Revsoft.Wabbitcode.GUI.DockingWindows.Tracking
         }
     }
 
-    internal class EnumByteKey
+    internal class ByteMaskKey
     {
-        public byte Byte { get; private set; }
+        private readonly byte _byte;
+        private readonly byte _mask;
 
-        public byte Mask { get; private set; }
-
-        public EnumByteKey(byte byteValue, byte maskValue)
+        public ByteMaskKey(byte byteValue, byte maskValue)
         {
-            Byte = byteValue;
-            Mask = maskValue;
+            _byte = byteValue;
+            _mask = maskValue;
+        }
+
+        public override int GetHashCode()
+        {
+            return (_byte << 8) + _mask;
+        }
+
+        public override bool Equals(object obj)
+        {
+            ByteMaskKey key = obj as ByteMaskKey;
+            if (key == null)
+            {
+                return false;
+            }
+
+            return key._byte == _byte && key._mask == _mask;
         }
     }
 }
