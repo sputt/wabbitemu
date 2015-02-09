a03ce9a4fbffd621e8d8aeed92c0d018943519cb
BuckeyeDude_cp <SND\BuckeyeDude_cp>
2015-02-09 12:40:49 +0000

    Adding missing file

--- WabbitStudio/Source/WPFZ80MapEditor/WPFZ80MapEditor/MainWindow.xaml.vb
+++ WabbitStudio/Source/WPFZ80MapEditor/WPFZ80MapEditor/MainWindow.xaml.vb
@@ -1,96 +1,44 @@
 ﻿Imports System.Windows.Media.Animation
 Imports System.Linq
 Imports System.IO
-Imports WPFZ80MapEditor.ValueConverters
 Imports Microsoft.Win32
+Imports WPFZ80MapEditor.ValueConverters
 
-<TemplatePart(Name:="PART_LayerContainer", Type:=GetType(XLayerContainer))>
 Public Class MainWindow
-    Public Shared Property ZeldaFolder As String
-    Public Shared Property RomPath As String
-
-    Public Shared Instance As MainWindow
-
-    Private _Model As New AppModel
-    Public ReadOnly Property Model As AppModel
+    Private ReadOnly Property Model As AppModel
         Get
-            Return _Model
+            Return MapControl.Model
         End Get
     End Property
 
     Public Sub New()
-        Instance = Me
         InitializeComponent()
+
+        DataContext = Model
     End Sub
 
-    Private Async Sub Window_Loaded(sender As Object, e As RoutedEventArgs) Handles MyBase.Loaded
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs) Handles MyBase.Loaded
         DataContext = Model
         Model.CurrentLayer = LayerType.MapsetLayer
 
-        RomPath = Registry.GetValue("HKEY_CURRENT_USER\Software\Wabbitemu", "rom_path", "")
+        MapEditorControl.RomPath = Registry.GetValue("HKEY_CURRENT_USER\Software\Wabbitemu", "rom_path", "")
+        ObjectsPanel.DragScope = MapControl
 
-        If ZeldaFolder IsNot Nothing Then
-            Dim HillScenario As New Scenario
-            Await HillScenario.LoadScenario(Path.Combine(ZeldaFolder, "maps\hill.asm"))
-
-            Debug.Print("Done loading: " & (Now - DateTime.FromFileTime(Application.StartTime)).TotalMilliseconds & " ms")
-            AddHandler LayerContainer.LayoutUpdated, AddressOf LayoutChanged
-            Model.Scenario = HillScenario
+        If MapEditorControl.ZeldaFolder IsNot Nothing Then
+            Dim fileName = Path.Combine(MapEditorControl.ZeldaFolder, "maps\hill.asm")
+            MapControl.OpenScenario(fileName)
         End If
 
-        LayerContainer.AddHandler(MapLayer.CoordinatesUpdatedEvent, New RoutedEventHandler(Sub(ctrl As Object, args As CoordinatesUpdatedArgs)
-                                                                                               Model.Status = args.Point.X & ", " & args.Point.Y
-                                                                                           End Sub))
+        MapControl.LayerContainer.AddHandler(MapLayer.CoordinatesUpdatedEvent, New RoutedEventHandler(Sub(ctrl As Object, args As CoordinatesUpdatedArgs)
+                                                                                                          Model.Status = args.Point.X & ", " & args.Point.Y
+                                                                                                      End Sub))
     End Sub
-
-    Private Sub LayoutChanged(sender As Object, e As Object)
-        Debug.Print("Done with layout: " & (Now - DateTime.FromFileTime(Application.StartTime)).TotalMilliseconds & " ms")
-        RemoveHandler LayerContainer.LayoutUpdated, AddressOf LayoutChanged
-    End Sub
-
-    Private Sub Background_MouseWheel(sender As Object, e As MouseWheelEventArgs)
-        Const DefaultZoomFactor As Double = 1.4
-        Dim zoomFactor = DefaultZoomFactor
-
-        Dim physicalPoint As Point = e.GetPosition(sender)
-
-        Dim RenderTransform As TransformGroup = Resources("MapCanvasRenderTransform")
-        Dim tt As TranslateTransform = RenderTransform.Children.First(Function(t) TypeOf t Is TranslateTransform)
-        Dim st As ScaleTransform = RenderTransform.Children.First(Function(t) TypeOf t Is ScaleTransform)
-
-        Dim mousePosition = RenderTransform.Inverse.Transform(physicalPoint)
-
-        If e.Delta <= 0 Then zoomFactor = 1.0 / DefaultZoomFactor
-        Dim currentZoom = st.ScaleX
-
-        Dim conv = New DoubleToZoomLevelConverter
-        currentZoom *= zoomFactor
-
-        currentZoom = Math.Min(8.0, Math.Max(0.5, currentZoom))
-
-        tt.BeginAnimation(TranslateTransform.XProperty, CreateZoomAnimation(-1 * (mousePosition.X * currentZoom - physicalPoint.X)), HandoffBehavior.Compose)
-        tt.BeginAnimation(TranslateTransform.YProperty, CreateZoomAnimation(-1 * (mousePosition.Y * currentZoom - physicalPoint.Y)), HandoffBehavior.Compose)
-
-        st.BeginAnimation(ScaleTransform.ScaleXProperty, CreateZoomAnimation(currentZoom), HandoffBehavior.Compose)
-        st.BeginAnimation(ScaleTransform.ScaleYProperty, CreateZoomAnimation(currentZoom), HandoffBehavior.Compose)
-
-        If currentZoom < 2.0 Then
-            RenderOptions.SetBitmapScalingMode(LayerContainer, BitmapScalingMode.Fant)
-            RenderOptions.SetBitmapScalingMode(TestView, BitmapScalingMode.Fant)
-        Else
-            RenderOptions.SetBitmapScalingMode(LayerContainer, BitmapScalingMode.NearestNeighbor)
-            RenderOptions.SetBitmapScalingMode(TestView, BitmapScalingMode.NearestNeighbor)
-        End If
-    End Sub
-
-    Private StartPoint As Point
-    Private Origin As Point
 
     Private Sub ZoomAnimationEnd(sender As Object, e As EventArgs)
         Dim clock As Clock = sender
 
         If clock.CurrentState <> ClockState.Active Then
-            Dim RenderTransform As TransformGroup = Resources("MapCanvasRenderTransform")
+            Dim RenderTransform As TransformGroup = MapControl.Resources("MapCanvasRenderTransform")
             Dim st As ScaleTransform = RenderTransform.Children.First(Function(t) TypeOf t Is ScaleTransform)
             Dim conv = New DoubleToZoomLevelConverter
             CurrentZoomLevelItem.Content = conv.Convert(st.ScaleX, GetType(String), Nothing, Nothing)
@@ -107,29 +55,6 @@ Public Class MainWindow
         Return da
     End Function
 
-    Private Sub Background_MouseMove(sender As Object, e As MouseEventArgs)
-        Dim MapCanvas As Canvas = sender
-        If MapCanvas.IsMouseCaptured Then
-
-            Dim RenderTransform As TransformGroup = Resources("MapCanvasRenderTransform")
-
-            Dim tt As TranslateTransform = RenderTransform.Children.First(Function(t) TypeOf t Is TranslateTransform)
-
-            Dim physicalPoint = e.GetPosition(sender)
-
-            Dim toX = physicalPoint.X - StartPoint.X + Origin.X
-            Dim toY = physicalPoint.Y - StartPoint.Y + Origin.Y
-
-            If tt.HasAnimatedProperties Then
-                tt.ApplyAnimationClock(TranslateTransform.XProperty, Nothing)
-                tt.ApplyAnimationClock(TranslateTransform.YProperty, Nothing)
-            End If
-
-            tt.X = toX
-            tt.Y = toY
-        End If
-    End Sub
-
     Private Sub ZoomLevelCombo_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles ZoomLevelCombo.SelectionChanged
         Dim combo As ComboBox = sender
 
@@ -143,12 +68,12 @@ Public Class MainWindow
         End Try
 
         Try
-            Dim RenderTransform As TransformGroup = Resources("MapCanvasRenderTransform")
-            If Not LayerContainer Is Nothing AndAlso Not RenderTransform Is Nothing Then
+            Dim RenderTransform As TransformGroup = MapControl.Resources("MapCanvasRenderTransform")
+            If Not MapControl.LayerContainer Is Nothing AndAlso Not RenderTransform Is Nothing Then
                 Dim tt As TranslateTransform = RenderTransform.Children.First(Function(t) TypeOf t Is TranslateTransform)
                 Dim st As ScaleTransform = RenderTransform.Children.First(Function(t) TypeOf t Is ScaleTransform)
 
-                Dim PhysPos As New Point(MapCanvas.ActualWidth / 2, MapCanvas.ActualHeight / 2)
+                Dim PhysPos As New Point(MapControl.MapCanvas.ActualWidth / 2, MapControl.MapCanvas.ActualHeight / 2)
                 Dim MousePos = RenderTransform.Inverse.Transform(PhysPos)
                 tt.BeginAnimation(TranslateTransform.XProperty, CreateZoomAnimation(-1 * (MousePos.X * newzoom - PhysPos.X)), HandoffBehavior.Compose)
                 tt.BeginAnimation(TranslateTransform.YProperty, CreateZoomAnimation(-1 * (MousePos.Y * newzoom - PhysPos.Y)), HandoffBehavior.Compose)
@@ -159,14 +84,6 @@ Public Class MainWindow
         Catch ex As Exception
 
         End Try
-    End Sub
-
-    Private Sub ZoomLevelCombo_DropDownClosed(sender As Object, e As EventArgs) Handles ZoomLevelCombo.DropDownClosed
-        CurrentZoomLevelItem.ClearValue(HeightProperty)
-    End Sub
-
-    Private Sub ZoomLevelCombo_DropDownOpened(sender As Object, e As EventArgs) Handles ZoomLevelCombo.DropDownOpened
-        CurrentZoomLevelItem.Height = 0
     End Sub
 
     Private Sub MapsetNew_Click(sender As Object, e As RoutedEventArgs)
@@ -188,45 +105,6 @@ Public Class MainWindow
         Model.Scenario.RemoveMap(Model.SelectedMap)
     End Sub
 
-    Private Sub NewScenario_Click(sender As Object, e As RoutedEventArgs)
-        Dim dlg As New Windows.Forms.OpenFileDialog
-        If dlg.ShowDialog() = Forms.DialogResult.OK Then
-            ZeldaFolder = Path.GetDirectoryName(dlg.FileName)
-
-            Dim Scenario As New Scenario
-            Model.Scenario = Scenario
-        End If
-    End Sub
-
-    'Private Sub TestButton_Click(sender As Object, e As RoutedEventArgs) Handles TestButton.Click
-    '    Dim GameWindow As New GameWindow()
-    '    GameWindow.Scenario = Scenario
-    '    GameWindow.Show()
-    'End Sub
-
-    Private Sub Background_PreviewMouseRightButtonDown(sender As Object, e As MouseButtonEventArgs)
-        Dim RenderTransform As TransformGroup = Resources("MapCanvasRenderTransform")
-        Dim tt As TranslateTransform = RenderTransform.Children.First(Function(t) TypeOf t Is TranslateTransform)
-
-        StartPoint = e.GetPosition(sender)
-        Origin = New Point(tt.X, tt.Y)
-        sender.CaptureMouse()
-    End Sub
-
-    Private Sub Background_PreviewMouseRightButtonUp(sender As Object, e As MouseButtonEventArgs)
-        sender.ReleaseMouseCapture()
-    End Sub
-
-    Private Sub RadioMapSet_Click(sender As Object, e As RoutedEventArgs)
-        If sender.tag IsNot Nothing Then
-            Model.CurrentLayer = sender.Tag
-        End If
-    End Sub
-
-    Private Sub Map_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs)
-        MapsetNew_Click(sender, e)
-    End Sub
-
     Private Sub SaveCanExecute(sender As Object, e As CanExecuteRoutedEventArgs)
         e.CanExecute = True
         e.Handled = True
@@ -240,7 +118,7 @@ Public Class MainWindow
     Private Async Sub OpenExecuted(sender As Object, e As ExecutedRoutedEventArgs)
         Dim dlg As New Windows.Forms.OpenFileDialog
         If dlg.ShowDialog() = Forms.DialogResult.OK Then
-            ZeldaFolder = Path.GetDirectoryName(Path.GetDirectoryName(dlg.FileName))
+            MapEditorControl.ZeldaFolder = Path.GetDirectoryName(Path.GetDirectoryName(dlg.FileName))
 
             Model.DeselectAll()
 
@@ -282,12 +160,11 @@ Public Class MainWindow
 
         _OldLayer = Model.CurrentLayer
         Model.CurrentLayer = LayerType.TestingLayer
-        Debug.Print(Me.VisualStateGroup.CurrentState.Name)
 
         Model.GameModel = New GameModel(Model.Scenario)
         Model.GameModel.Start()
 
-        TestView.Focus()
+        MapControl.TestView.Focus()
     End Sub
 
     Public Sub StopTesting()
